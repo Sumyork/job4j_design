ba00064aeee5d0444da64d9c32e345c260a58760
Sumyork <psychohistorian.p@gmail.com>
2024-07-31 22:05:02 +0300

    3. Итератор четных чисел [#150] - practice

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,38 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator {
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return checkEvenNumber() == 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+
+    private int checkEvenNumber() {
+        int result = -1;
+        for (int i = index; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                index = i;
+                result++;
+                break;
+            }
+        }
+        return result;
+    }
+}
