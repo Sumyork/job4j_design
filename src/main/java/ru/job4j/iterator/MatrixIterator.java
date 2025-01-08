0299e37e50dd6963e07f9a2764eea0d57cc6986e
Sumyork <psychohistorian.p@gmail.com>
2025-01-09 02:29:55 +0300

    2. Итератор для двухмерного массива int[][] [#9539 #506955] - practice v.2

--- src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -14,14 +14,11 @@ public class MatrixIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while (column == data[row].length) {
-            if (row == data.length - 1) {
-                return false;
-            }
+        while (data[row].length == column && row < data.length - 1) {
             row++;
             column = 0;
         }
-        return true;
+        return column < data[row].length;
     }
 
     @Override
@@ -30,5 +27,26 @@ public class MatrixIterator implements Iterator<Integer> {
             throw new NoSuchElementException();
         }
         return data[row][column++];
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {
+                {1}, {2, 3}, {}, {}, {4}
+        };
+        int row = 0;
+        int column = 0;
+        MatrixIterator iterator = new MatrixIterator(input);
+        System.out.println(input[row++].length);
+        System.out.println(input[row++].length);
+        System.out.println(input[row++].length);
+        System.out.println(input[row++].length);
+        System.out.println(input[row++].length);
+//        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
     }
 }
