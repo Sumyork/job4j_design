3aeefc7d41ba5c051ba62dde22ea80784c75273a
Sumyork <psychohistorian.p@gmail.com>
2025-01-14 00:07:31 +0300

    6. Циклический итератор [#505082 #508635] - practice

--- src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -28,25 +28,4 @@ public class MatrixIterator implements Iterator<Integer> {
         }
         return data[row][column++];
     }
-
-    public static void main(String[] args) {
-        int[][] input = {
-                {1}, {2, 3}, {}, {}, {4}
-        };
-        int row = 0;
-        int column = 0;
-        MatrixIterator iterator = new MatrixIterator(input);
-        System.out.println(input[row++].length);
-        System.out.println(input[row++].length);
-        System.out.println(input[row++].length);
-        System.out.println(input[row++].length);
-        System.out.println(input[row++].length);
-//        System.out.println(iterator.hasNext());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-    }
 }
